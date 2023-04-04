570e2e5b6c059e1fcf90a26c5d2c6358ca06ca2f
5469380 <5469380@stu.kmu.ac.kr>
2023-04-04 17:32:12 +0900

    수정필요

--- 5469380_14_gameA/Program.cs
+++ 5469380_14_gameA/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace _5469380_14_gameA
 {
@@ -20,29 +21,38 @@ namespace _5469380_14_gameA
         public int y;
     }
 
+
+
     internal class Program
     {
-        static Item item = new Item();
+        static Item[] item;
+        static int level = 1;
+
         static Random random = new Random();
         static Player player = new Player();
         static void GenerateItem()
         {
-
-            while (true) //메소드로 빼면 좋다. 시험에선 메소드로 만들어야 함. 코드 리팩토링 과정에서 메소드를 만드는 것이 좋음.
+            item = new Item[level]; //레벨당 배열 만들기
+            for (int i = 0; i < level; i++)
             {
-                item.x = random.Next() % 70 + 5;
-                item.y = random.Next() % 20 + 2;
+                item[i] = new Item(); // 실제 공간이 만들어지는 곳 실제 객체화가 되는 곳임, 위에는 그냥 선언만 했음
 
-                if (item.x == player.x && item.y == player.y)
+                while (true) //메소드로 빼면 좋다. 시험에선 메소드로 만들어야 함. 코드 리팩토링 과정에서 메소드를 만드는 것이 좋음.
                 {
-                    continue;
-                }
-                else
-                    break;
-            }
+                    item[i].x = random.Next() % 70 + 5;
+                    item[i].y = random.Next() % 20 + 2;
 
-            Console.SetCursorPosition(item.x, item.y);
-            Console.WriteLine('*');
+                    if (item[i].x == player.x && item[i].y == player.y)
+                    {
+                        continue;
+                    }
+                    else
+                        break;
+                }
+
+                Console.SetCursorPosition(item[i].x, item[i].y);
+                Console.WriteLine('*');
+            }
         }
         static void Main(string[] args)
         {
@@ -60,13 +70,34 @@ namespace _5469380_14_gameA
             Console.SetCursorPosition(50, 0); //글자 위치, 밑에 WriteLine을 사용하면 해당위치에 적힌다.
             Console.WriteLine(String.Format("Score : {0:00000}", score));
 
-            while(true)
+            Console.SetCursorPosition(5, 0);
+            Console.WriteLine(String.Format("Stage : {0}", level));
+
+            var startTime = DateTime.Now;
+            TimeSpan totalTime = new TimeSpan(0, 0, 60); //기본적으로 datetime 클래스는 timespan 변수를 사용하므로 이를 간섭하려면 timespan 클래스의 변수를 사용해야함.
+
+            int count = level;
+
+            while (true)
             {
+                int remainTime = (int)(totalTime - DateTime.Now.Subtract(startTime)).TotalSeconds;
+
+                if (remainTime <= 0)
+                {
+                    Console.SetCursorPosition(30, 10);
+                    Console.Write("game over");
+                    break;
+                }
+
+
                 Console.SetCursorPosition(0, 0);
-                Console.Write(DateTime.Now.ToString()); //현재시간 출력
+                Console.Write(remainTime.ToString() + "초");
 
                 if (!Console.KeyAvailable) //현재시간은 커서가 움직이는 걸 기준으로 출력되게 되지만 이 조건문을 쓰면 실시간으로 출력된다.
+                {
+                    Thread.Sleep(1); //system.threading 을 사용해서 cpu누수를 막을 수 있다.   
                     continue;
+                }
 
                 var Key = Console.ReadKey(); //방향키를 통해 이동가능
 
@@ -113,20 +144,35 @@ namespace _5469380_14_gameA
                 else if(Key.Key == ConsoleKey.Escape) //esc 종료
                     break;
 
-                if(player.x == item.x && player.y == item.y) // * 을 먹으면 10점씩 추가 반드시 if문으로.
+
+                for (int i = 0; i < level; i++)
                 {
-                    score += 10;
+
+                    if (player.x == item[i].x && player.y == item[i].y) // * 을 먹으면 10점씩 추가 반드시 if문으로.
+                    {
+                        count--;
+
+                        level++;
+                        score += 10;
+
+                        Console.SetCursorPosition(5, 0);
+                        Console.WriteLine(String.Format("Stage : {0}", level));
+
+                        if (score == 50) // 50점 달성시 종료
+                        {
+                            Console.WriteLine("Clear");
+                            break;
+                        }
+                    }
+                }
+
+                if(count == 0)
+                {
                     Console.SetCursorPosition(50, 0);
                     Console.WriteLine(String.Format("Score : {0:00000}", score));
 
-                    if (score == 50) // 50점 달성시 종료
-                    {
-                        
-                        Console.WriteLine("Clear");
-                        break;
-                    }
+                    GenerateItem();
 
-                    GenerateItem(); //* 아이템을 만드는 함수
                 }
             }
         }
